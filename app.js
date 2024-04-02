bffa6671481d23c800cd5ca7dfcb8d478440b8a0
nickhumberstone <nickhumberstone@gmail.com>
2024-04-02 06:33:14 +0100

    removed non-existing imports

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-import { getAnswers, addAnswer, getAllAnswers, getDailyAnswers, getMyAnswers, getDailyQuestion, createUserProfile } from './databaselogic.js';
+import { addAnswer, getDailyAnswers, getMyAnswers, getDailyQuestion, createUserProfile } from './databaselogic.js';
 
 const app = express();
 app.use(cors());
