5712b558d15b4db9753023ebcec4fccbee5101cd
nickhumberstone <nickhumberstone@gmail.com>
2024-07-01 22:29:18 +0100

    updated schema, added docker.yml

---
+++ notificationScripts/communityResponsesReady.js
@@ -0,0 +1,5 @@
+// Script to be run once 11th response of the day is submitted
+// Process:
+// - 11th, not 10th query because you want each of those 11 notified to be able to see 10 other responses (not their own!)
+// - Triggers on 11th response, and tries to pull all the expopushtokens. If user's dont have expopushtokens, they just won't be included in the pool
+
