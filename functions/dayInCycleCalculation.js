bd3fdac3511e58d6b97a79dcb0c2173f3f61bc1a
nickhumberstone <nickhumberstone@gmail.com>
2024-09-05 16:54:21 +0100

    fixing export default for dayincycle

--- functions/dayInCycleCalculation.js
+++ functions/dayInCycleCalculation.js
@@ -1,8 +1,8 @@
-function dayInCycleCalculation(){
-    const startDate= new Date('2024-01-01');
-    const todayDate = new Date();
-    const daysDifference = Math.floor((todayDate - startDate))
-    console.log(Math.floor((todayDate - startDate)))
-    // return daysDifference
-    // 1-28
+export default function dayInCycleCalculation() {
+  const startDate = new Date("2024-01-01");
+  const todayDate = new Date();
+  const daysDifference = Math.floor(todayDate - startDate);
+  console.log(Math.floor(todayDate - startDate));
+  // return daysDifference
+  // 1-28
 }
